f026b5d90cb98d2c95fb24b69e4bef0df5d08a18
Himesh(Git) <adithra_git@outlook.com>
2019-09-26 15:47:08 +0530

    Main checkin

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,114 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.lblG1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblOS1 = New System.Windows.Forms.Label()
+        Me.lblIS1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblG1
+        '
+        Me.lblG1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.lblG1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblG1.Location = New System.Drawing.Point(27, 47)
+        Me.lblG1.Name = "lblG1"
+        Me.lblG1.Size = New System.Drawing.Size(20, 200)
+        Me.lblG1.TabIndex = 0
+        Me.lblG1.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(2, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Add Glass"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'lblOS1
+        '
+        Me.lblOS1.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.lblOS1.Location = New System.Drawing.Point(16, 47)
+        Me.lblOS1.Name = "lblOS1"
+        Me.lblOS1.Size = New System.Drawing.Size(10, 200)
+        Me.lblOS1.TabIndex = 0
+        Me.lblOS1.Visible = False
+        '
+        'lblIS1
+        '
+        Me.lblIS1.BackColor = System.Drawing.Color.LightCoral
+        Me.lblIS1.Location = New System.Drawing.Point(48, 47)
+        Me.lblIS1.Name = "lblIS1"
+        Me.lblIS1.Size = New System.Drawing.Size(10, 200)
+        Me.lblIS1.TabIndex = 0
+        Me.lblIS1.Visible = False
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Location = New System.Drawing.Point(0, 28)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 422)
+        Me.Panel1.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(186, 2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(83, 2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(97, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Add consumable"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.lblIS1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblOS1)
+        Me.Controls.Add(Me.lblG1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents lblG1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents lblOS1 As Label
+    Friend WithEvents lblIS1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,173 @@
 ﻿Public Class Form1
+    Public startX As Integer = 48
+    Public startY As Integer = 47
+    Dim MouseDownLocation As Point
+    Dim newController As Label
+    Dim counter As Integer = 1
+    Dim cCounter As Integer = 1
+    Dim canAddedGlass As Boolean = True
+    Dim isGlassAdded As Boolean = False
+    Dim isConAdded As Boolean = False
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            If canAddedGlass = True Then
+                For o = 1 To 3
+                    CreateLables(o, counter)
+                Next
+                counter += 1
+                canAddedGlass = False
+                isGlassAdded = True
+            Else
+                MessageBox.Show("Add consumable first")
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Function CreateLables(ByRef type As Integer, counter As Integer)
+        Try
+            Dim cName As String = "lbl99"
+            Dim cHeight As Integer = 300
+            Dim cWidth As Integer = 1
+            Dim cColor As Color = Color.Red
+            If type = 2 Then
+                cName = "lblG" & counter
+                cWidth = 20
+                cColor = Color.DodgerBlue
+            ElseIf type = 1 Then
+                cName = "lblSO" & counter
+                If isConAdded = True Then
+                    startX += 70
+                Else
+                    startX += 40
+                End If
+                cWidth = 10
+                cColor = Color.DarkTurquoise
+            ElseIf type = 3 Then
+                cName = "lblSI" & counter
+                cWidth = 10
+                cColor = Color.LightCoral
+                startX += 10
+            ElseIf type = 4 Then
+                cName = "lblC" & cCounter
+                cWidth = 40
+                cColor = Color.Red
+                startX += 40
+            End If
+            newController = New Label
+            newController.Name = cName
+            newController.Text = ""
+            newController.AutoSize = False
+            newController.Height = cHeight
+            newController.Width = cWidth
+            newController.BackColor = cColor
+            newController.Location = New Point(startX + 10.5, 47)
+            newController.FlatStyle = FlatStyle.Flat
+            AddHandler newController.MouseClick, AddressOf LblG1_MouseDown
+            AddHandler newController.MouseMove, AddressOf LblG1_MouseMove
+            AddHandler newController.MouseLeave, AddressOf LblG1_MouseLeave
+            AddHandler newController.Click, AddressOf MyFunc2
+            AddHandler newController.MouseEnter, AddressOf showItem
+            newController.BringToFront()
+            Panel1.Controls.Add(newController)
+            startX = newController.Left
+        Catch ex As Exception
+
+        End Try
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            startX = lblIS1.Left
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub LblG1_MouseDown(sender As Object, e As MouseEventArgs) Handles lblG1.MouseDown
+        Try
+            If e.Button = System.Windows.Forms.MouseButtons.Left Then
+                MouseDownLocation = e.Location
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub LblG1_MouseMove(sender As Object, e As MouseEventArgs) Handles lblG1.MouseMove
+        Try
+            newController = New Label
+            newController = DirectCast(sender, Label)
+            If (e.Button = System.Windows.Forms.MouseButtons.Left) Then
+                newController.Left = e.X + newController.Left - MouseDownLocation.X
+                newController.Top = e.Y + newController.Top - MouseDownLocation.Y
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub LblG1_MouseLeave(sender As Object, e As EventArgs) Handles lblG1.MouseLeave
+        Try
+            newController = New Label
+            newController = DirectCast(sender, Label)
+            newController.Location = New Point(newController.Location.X, 47)
+            newController.BorderStyle = BorderStyle.None
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Sub MyFunc2(sender As Object, e As EventArgs)
+        Try
+            newController = New Label
+            newController = DirectCast(sender, Label)
+            Dim Item As String
+            Dim ItemNumber As String
+            Item = newController.Name
+            If Item.Contains("G") Then
+
+            End If
+            MessageBox.Show("This is " & newController.Name)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Panel1.Controls.Clear()
+        canAddedGlass = True
+        startX = 48
+        counter = 1
+        cCounter = 1
+        isGlassAdded = False
+        isConAdded = False
+    End Sub
+
+    Private Sub showItem(sender As Object, e As EventArgs)
+        Try
+            newController = New Label
+            newController = DirectCast(sender, Label)
+            newController.BorderStyle = BorderStyle.FixedSingle
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            If isGlassAdded = True Then
+                CreateLables(4, cCounter)
+                cCounter += 1
+                canAddedGlass = True
+                isConAdded = True
+            Else
+                MessageBox.Show("Add glass first")
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
 End Class
